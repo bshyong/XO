57ad30157b652b8def952dcd93d7746942356b13
Benjamin Shyong <ben.shyong@gmail.com>
2014-06-27 03:13:08 -0700

    Initial Commit

---
+++ XO/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  XO
+//
+//  Created by Benjamin Shyong on 6/27/14.
+//  Copyright (c) 2014 ShyongTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XO
+//
+//  Created by Benjamin Shyong on 6/27/14.
+//  Copyright (c) 2014 ShyongTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
