1f704bb99f3e88b32f5abd44deecbd5587d68fd4
Benjamin Shyong <ben.shyong@gmail.com>
2014-06-29 16:35:00 -0700

    add images and customize UI nav bar

--- XO/STAppDelegate.m
+++ XO/STAppDelegate.m
@@ -14,9 +14,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self customizeUserInterface];
     [Parse setApplicationId:@"GmyprQlm3id1kRwk8t1mRJXEEATehVBggJ3U2gBI"
                 clientKey:@"0Bm0pcbNwSF3CW6wRCOunB7vUfmrgWmUW0z37zeq"];
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+  
     return YES;
 }
 
@@ -45,6 +47,14 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+# pragma mark - Helper Methods
+-(void)customizeUserInterface{
+//  [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0.553 green:0.435 blue:0.718 alpha:1.0]];
+  [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed:@"navBarBackground"] forBarMetrics:UIBarMetricsDefault];
+  [[UINavigationBar appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], NSForegroundColorAttributeName, nil]];
+  [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
 }
 
 @end
